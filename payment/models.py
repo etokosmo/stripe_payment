6fa60997941f54a1893b3be267a05bb9229c07c3
Denis Polyakov <p-ov.da@ya.ru>
2022-11-23 21:35:14 +0300

    Add Item Model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(
+        verbose_name="Имя",
+        max_length=200,
+    )
+    description_short = models.TextField(
+        verbose_name="Описание",
+        blank=True
+    )
+    price = models.DecimalField(
+        verbose_name="Стоимость",
+        max_digits=10,
+        decimal_places=1,
+        validators=[MinValueValidator(0)]
+    )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
