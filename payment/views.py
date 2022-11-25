edd02268f72827e728ee7d65320e08d81f561a1d
Denis Polyakov <p-ov.da@ya.ru>
2022-11-25 21:32:20 +0300

    Add payment to item

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,8 @@
 import stripe
-from django.http import HttpResponse
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from .models import Item
@@ -10,11 +11,11 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class SuccessView(TemplateView):
-    template_name = "success.html"
+    template_name = "payment/success.html"
 
 
 class CancelView(TemplateView):
-    template_name = "cancel.html"
+    template_name = "payment/cancel.html"
 
 
 def create_checkout_session(request, item_id):
@@ -23,11 +24,11 @@ def create_checkout_session(request, item_id):
         session = stripe.checkout.Session.create(
             line_items=[{
                 'price_data': {
-                    'currency': 'usd',
+                    'currency': 'rub',
                     'product_data': {
                         'name': item.name,
                     },
-                    'unit_amount': item.price,
+                    'unit_amount': item.price * 100,
                 },
                 'quantity': 1,
             }],
@@ -35,8 +36,11 @@ def create_checkout_session(request, item_id):
             success_url=request.build_absolute_uri('/api/success'),
             cancel_url=request.build_absolute_uri('/api/cancel'),
         )
-        return HttpResponse(session.stripe_id)
+        return JsonResponse({'sessionId': session.stripe_id})
 
 
-def get_item(request):
-    pass
+def get_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+
+    context = {'item': item, 'stripe_public_key': settings.STRIPE_PUBLIC_KEY}
+    return render(request, "payment/item.html", context=context)
